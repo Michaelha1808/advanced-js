afac18ceba50308b219b489f13e6739f11764f08
Michaelha1808 <haminhchi1808@gmail.com>
2024-03-14 15:23:48 +0700

    update closure and IIFE

--- Closure/index.js
+++ Closure/index.js
@@ -1,33 +1,33 @@
-// function createCounter() {
-//   let counter = 0;
-//   function increase() {
-//     return ++counter;
-//   }
-//   return increase;
-// }
-// const counter1 = createCounter();
-// console.log(counter1());
-// console.log(counter1());
-// console.log(counter1());
+function createCounter() {
+  let counter = 0;
+  function increase() {
+    return ++counter;
+  }
+  return increase;
+}
+const counter1 = createCounter();
+console.log(counter1());
+console.log(counter1());
+console.log(counter1());
 
-// function createLogger(namwspace) {
-//   function logger(message) {
-//     console.log(`[${namwspace}] ${message}`);
-//   }
-//   return logger;
-// }
-// // ======== app =================================================
-// const inforLogger = createLogger("Info");
+function createLogger(namwspace) {
+  function logger(message) {
+    console.log(`[${namwspace}] ${message}`);
+  }
+  return logger;
+}
+// ======== app =================================================
+const inforLogger = createLogger("Info");
 
-// inforLogger("Bắt đầu gửi mail");
-// inforLogger("Gửi mail lỗi lần 1, thử lại...");
-// inforLogger("Gửi mail thành công cho user xxx");
+inforLogger("Bắt đầu gửi mail");
+inforLogger("Gửi mail lỗi lần 1, thử lại...");
+inforLogger("Gửi mail thành công cho user xxx");
 
-// const errorLogger = createLogger("Error");
+const errorLogger = createLogger("Error");
 
-// errorLogger("Email không tồn tại");
-// errorLogger("Gửi mail lỗi lần 1, thử lại...");
-// errorLogger("Gửi mail thành công cho user xxx");
+errorLogger("Email không tồn tại");
+errorLogger("Gửi mail lỗi lần 1, thử lại...");
+errorLogger("Gửi mail thành công cho user xxx");
 
 function createStorage(key) {
   const store = JSON.parse(localStorage.getItem(key)) ?? {};
--- IIFE/index.js
+++ IIFE/index.js
@@ -1,25 +1,39 @@
-// let fullname = "Miichael";
-// (function myfunc(e) {
-//   console.log("dâdad", e);
-// })(fullname);
+let fullname = "Miichael";
+(function myfunc(e) {
+  console.log("dâdad", e);
+})(fullname);
 
-// const app = (function () {
-//   const cars = [];
-//   return {
-//     add(car) {
-//       cars.push(car);
-//     },
-//     edit(index, car) {
-//       cars[index] = car;
-//     },
-//     delete(index) {
-//       cars.splice(index, 1);
-//     },
-//   };
-// })();
+const app = (function () {
+  const cars = [];
+  return {
+    add(car) {
+      cars.push(car);
+    },
+    edit(index, car) {
+      cars[index] = car;
+    },
+    delete(index) {
+      cars.splice(index, 1);
+    },
+  };
+})();
 let b = 0;
 if (b) {
   console.log(1);
 } else {
   console.log(2);
 }
+
+(function js(x) {
+  const y = (j) => j * x;
+
+  console.log(y(s()));
+
+  function s() {
+    return j();
+  }
+
+  function j() {
+    return x ** x;
+  }
+})(3);
