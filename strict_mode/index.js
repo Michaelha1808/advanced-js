b333224aad7f32bd14091c239047939eacaf0c67
Michaelha1808 <haminhchi1808@gmail.com>
2024-03-14 16:04:44 +0700

    add strict mode

---
+++ strict_mode/index.js
@@ -0,0 +1,25 @@
+"use strict";
+// fullname = "daddad";
+// function testFunc() {
+//   age = 18;
+// }
+// testFunc();
+
+// console.log(fullname);
+// console.log(age);
+
+// const student = {
+//   fullname: "abbd",
+// };
+// const student = Object.freeze({
+//   fullname: "abbd",
+// });
+// student.fullname = "hello world";
+// console.log(student);
+
+{
+  function sum(a, b) {
+    return a + b;
+  }
+}
+console.log(sum(9, 6));
